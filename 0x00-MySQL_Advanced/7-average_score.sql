09c583751a74179a9cfc7fc22a91c49c24ae7b87
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-08 20:23:55 +0300

    Update stored procedure to compute average score for a user

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,21 +6,14 @@
 -- user_id, a users.id value (you can assume user_id is linked to an existing users)
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 
-    -- Compute the average score for the given user_id
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update the average_score in the users table
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
 END //
 
 DELIMITER ;

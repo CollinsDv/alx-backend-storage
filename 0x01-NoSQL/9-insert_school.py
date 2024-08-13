e8fac830b0b1bb8dea46bf6413ea14e0612e8447
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-13 12:34:26 +0300

    refactor: Update insert_school function to use type annotations and improve code readability

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,19 +2,15 @@
 """module: 9-insert_school
 adds a document in a collection
 """
-from pymongo.collection import Collection
-from bson.objectid import ObjectId
 
 
-def insert_school(mongo_collection: Collection, **kwargs) -> ObjectId:
-    """Add a document to a collection.
-
+def insert_school(mongo_collection, **kwargs):
+    """add a document to a collection
     Args:
-        mongo_collection (Collection): The pymongo collection object.
-        kwargs (dict): Dictionary for the document.
-
+        mongo_collection (Collection) -> mongo collection object
+        kwargs (dict) -> dictionary for document
     Returns:
-        ObjectId: The _id of the new mongo collection object.
+        ObjectId -> _id of the new mongo collection object
     """
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id

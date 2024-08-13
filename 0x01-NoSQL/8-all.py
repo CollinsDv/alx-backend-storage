db1b652ad267d8d0cdace12e575c5de94ec347ad
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-13 12:04:21 +0300

    reduce number of lines

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 """module 8-all
+lists all documents in collection in python
 """
 from pymongo.collection import Collection
-# Python function that lists all documents in a collection:
-
-# Prototype: def list_all(mongo_collection):
-# Return an empty list if no document in the collection
-# mongo_collection will be the pymongo collection object
 
 
 def list_all(mongo_collection: Collection) -> list:

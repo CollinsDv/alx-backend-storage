6c77318e5fa5541155879af28348df498883bb8a
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-13 12:27:01 +0300

    handle pep

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,13 +5,14 @@ adds a document in a collection
 from pymongo.collection import Collection
 from bson.objectid import ObjectId
 
+
 def insert_school(mongo_collection: Collection, **kwargs) -> ObjectId:
     """Add a document to a collection.
-    
+
     Args:
         mongo_collection (Collection): The pymongo collection object.
         kwargs (dict): Dictionary for the document.
-    
+
     Returns:
         ObjectId: The _id of the new mongo collection object.
     """

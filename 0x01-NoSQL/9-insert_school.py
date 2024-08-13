749d857dacfd139579715abd515af54d8171cb03
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-13 12:21:05 +0300

    feat: Add function to insert a school document in the collection

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module: 9-insert_school
+adds a document in a collection
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """add a document to a collection
+    Args:
+        mongo_collection (Collection) -> mongo collection object
+        kwargs (dict) -> dictionary for document
+    Returns:
+        ObjectId -> _id of the new mongo collection object
+    """
+    result = mongo_collection.insertOne(kwargs)
+    return result.inserted_id

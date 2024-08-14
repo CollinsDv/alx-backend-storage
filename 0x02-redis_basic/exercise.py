fffe82238288e5c303cba0f74a02e65f621a0f55
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-14 20:31:08 +0300

    Refactor Cache class to include get method for retrieving data from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,37 +1,48 @@
-#!/usr/bin/env python3
-"""module: exercise
-Create a Cache class. In the __init__ method, store an instance
-of the Redis client as a private variable named _redis (using
-redis.Redis()) and flush the instance using flushdb.
-
-Create a store method that takes a data argument and returns
-a string. The method should generate a random key (e.g. using
-uuid), store the input data in Redis using the random key and
-return the key.
-
-Type-annotate store correctly. Remember that data can be a str,
-bytes, int or float.
-"""
 import redis
-from typing import Union
 from uuid import uuid4
+from typing import Union, Callable, Optional, Any
 
-
-class Cache():
-    """A class initializing a redix instance"""
+class Cache:
+    """A class initializing a Redis instance"""
     def __init__(self) -> None:
-        """creates a redis inatance"""
+        """Creates a Redis instance"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """store input data in Redis with a random key
+        """Store input data in Redis with a random key
         Args:
-            data (str, bytes, int, float) -> data to be stored
+            data (str, bytes, int, float): Data to be stored
 
         Returns:
-            str: the random key
+            str: The random key
         """
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """ Gets key's value from redis and converts
+            result byte  into correct data type
+        """
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if callable(fn):
+            return fn(value)
+        return value
+
+    def get_str(self, data: bytes) -> str:
+        """ Converts bytes to string
+        """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """ Converts bytes to integers
+        """
+        return int(data)
